c8f1cdb1decc62f8b2c5045cbdd5729d91cda82a
beniwalsanju <sanjuboi0306@gmail.com>
2026-09-23 21:54:13 +0530

    I make a basic Calculator

---
+++ src/sanju/Calculator.java
@@ -0,0 +1,43 @@
+package sanju;
+
+import java.util.Scanner;
+
+public class Calculator {
+    static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int ans = 0;
+        while(true){
+//            Take the operator as input
+            System.out.print("Enter the operator: ");
+            int op = in.next().trim().charAt(0);
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%'){
+                // input two number
+                System.out.print("Enter two number:");
+                int num1 = in.nextInt();
+                int num2 = in.nextInt();
+                if (op == '+'){
+                    ans = num1 + num2;
+                }
+                if (op == '-') {
+                    ans = num1 - num2;
+                }
+                if (op == '*') {
+                    ans = num1 * num2;
+                }
+                if (op == '/'){
+                    if (num2 != 0){
+                        ans = num1 / num2;
+                    }
+                }
+                if (op == '%'){
+                    ans = num1 % num2;
+                }
+            } else if (op == 'x' || op == 'X') {
+                break;
+            }else {
+                System.out.println("Invalide opreator!!");
+            }
+        System.out.println(ans);
+        }
+    }
+}
